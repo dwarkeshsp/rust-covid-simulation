270870aa9e40d0890cd6f7106a4ecf69f301e15e
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-05-30 17:06:30 -0500

    calling people from mainstate

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,51 @@ use ggez::{Context, GameResult};
 
 mod person;
 
+struct MainState {
+    people: Vec<person::Person>,
+    width: f32,
+    height: f32,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let (width, height) = graphics::drawable_size(ctx);
+
+        let people = person::create_people(10, width, height);
+
+        let s = MainState {
+            people: people,
+            width: width,
+            height: height,
+        };
+
+        Ok(s)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        for p in &mut self.people {
+            person::update_person(p)
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
+        for p in &self.people {
+            person::draw_person(ctx, p)?;
+        }
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+
 pub fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("super_simple", "ggez");
     let (ctx, event_loop) = &mut cb.build()?;
-    let state = &mut person::Person::new(person::Status::Sick)?;
+    let state = &mut MainState::new(ctx)?;
     event::run(ctx, event_loop, state)
 }
--- src/person.rs
+++ src/person.rs
@@ -3,9 +3,10 @@ use ggez::event;
 use ggez::graphics;
 use ggez::nalgebra as na;
 use ggez::{Context, GameResult};
+use rand::Rng;
 
 #[derive(PartialEq)]
-pub enum Status {
+enum Status {
     Sick,
     Healthy,
 }
@@ -13,8 +14,47 @@ pub enum Status {
 pub struct Person {
     pos_x: f32,
     pos_y: f32,
+    dx: f32,
+    dy: f32,
     status: Status,
 }
+
+pub fn create_people(amount: i32, width: f32, heigth: f32) -> Vec<Person> {
+    let mut rng = rand::thread_rng();
+
+    let new_person = |_| {
+        let mut person = Person {
+            pos_x: width / 2.0,
+            pos_y: heigth / 2.0,
+            dx: 0.0,
+            dy: 0.0,
+            status: Status::Healthy,
+        };
+        person
+    };
+    (0..amount).map(new_person).collect()
+}
+
+pub fn update_person(person: &mut Person) {}
+
+pub fn draw_person(ctx: &mut Context, person: &Person) -> GameResult {
+    let color = if Status::Sick == person.status {
+        graphics::BLACK
+    } else {
+        graphics::WHITE
+    };
+    let circle = graphics::Mesh::new_circle(
+        ctx,
+        graphics::DrawMode::fill(),
+        na::Point2::new(0.0, 0.0),
+        20.0,
+        2.0,
+        color,
+    )?;
+    graphics::draw(ctx, &circle, (na::Point2::new(person.pos_x, person.pos_y),))?;
+    Ok(())
+}
+/*
 
 impl Person {
     pub fn new(status: Status) -> GameResult<Person> {
@@ -26,7 +66,6 @@ impl Person {
         Ok(s)
     }
 }
-
 impl event::EventHandler for Person {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         self.pos_x = self.pos_x % 800.0 + 1.0;
@@ -56,3 +95,4 @@ impl event::EventHandler for Person {
         Ok(())
     }
 }
+*/
